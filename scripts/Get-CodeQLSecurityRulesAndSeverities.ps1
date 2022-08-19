687c8ee72d862f40fac8743f1c9c0fd4b71eff91
David Wiggs <david-wiggs@github.com>
2022-08-19 12:57:50 -0500

    Initial upload of Get-CodeQLSecurityRulesAndSeverities.ps1

---
+++ scripts/Get-CodeQLSecurityRulesAndSeverities.ps1
@@ -0,0 +1,88 @@
+function Get-GitHubRepositoryFileContent {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $True)] [string] $gitHubRepository,
+        [Parameter(Mandatory = $True)] [string] $path,
+        [Parameter(Mandatory = $True)] [string] $branch,
+        [Parameter(Mandatory = $False)] [string] $token
+    )
+    $uri = "https://api.github.com/repos/$gitHubRepository/contents/$path`?ref=$branch" # Need to escape the ? that indicates an http query
+    $uri = [uri]::EscapeUriString($uri)
+    $splat = @{
+        Method = 'Get'
+        Uri = $uri
+        Headers = $headers
+        ContentType = 'application/json'
+    }
+    if ($PSBoundParameters.ContainsKey('token')) {
+        $headers = @{'Authorization' = "token $token"}
+        $splat.Add('Headers', $headers)
+    } 
+    
+    try {
+        $fileData = Invoke-RestMethod @splat
+        [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($fileData.content)) | Out-File -FilePath $(Split-Path $path -Leaf) -Force
+        Get-Item -Path $(Split-Path $path -Leaf)
+    } catch {
+        Write-Warning "Unable to get file content."   
+        $ErrorMessage = $_.Exception.Message
+        Write-Warning "$ErrorMessage"
+        break
+    }
+}
+
+function Get-DotSourceFileFromGitHub {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $True)] [string] $gitHubRepository,
+        [Parameter(Mandatory = $True)] [string] $path,
+        [Parameter(Mandatory = $True)] [string] $branch,
+        [Parameter(Mandatory = $False)] [string] $token
+    )
+    
+    $splat = @{
+        gitHubRepository = $gitHubRepository
+        path = $path
+        branch = $branch
+    }
+    if ($PSBoundParameters.ContainsKey('token')) {$splat.Add('token', $token)}
+    $dotSourcefile = Get-GitHubRepositoryFileContent @splat 
+    $content = Get-Content -Path $dotSourcefile.FullName 
+    $content.Replace('function ', 'function Global:') | Out-File $dotSourceFile.FullName -Force
+    . $dotSourcefile.FullName
+    Remove-Item -Path $dotSourcefile.FullName -Force
+}
+
+$splat = @{
+    gitHubRepository = 'david-wiggs/codeql-anywhere'
+    path = 'functions.ps1'
+    branch = 'main'
+}
+Get-DotSourceFileFromGitHub @splat
+
+$codeQLDirectory = Get-LatestCodeQLBundle
+$qlFiles = Get-ChildItem -Recurse -Path $codeQLDirectory -Include "*.ql" 
+foreach ($file in $qlFiles) {
+    $content = Get-Content -Path $file.FullName
+    if ($null -ne ($content | Select-String -Pattern '\* @security-severity')) {
+        $data = ($content | Select-String -Pattern '\* @') | ForEach-Object {$_.ToString()} | ForEach-Object {$_.Replace('* @', '') | ForEach-Object {$_.Trim()}}
+        if ($null -ne ($data | Where-Object {$_ -like 'precision*'})) {
+            $precision = ($data | Where-Object {$_ -like "precision*"}).Split()[-1]
+        } else {
+            $precision = 'N/A'
+        }
+
+        $dataObj = [PSCustomObject]@{
+            name = ($data | Where-Object {$_ -like "name*"}).Split() | Where-Object {$_ -notlike 'name'} | Join-String -Separator ' '
+            'security-severity' = ($data | Where-Object {$_ -like "security-severity*"}).Split()[-1] 
+            precision = $precision
+            id = ($data | Where-Object {$_ -like "id*"}).Split()[-1]
+            language = ($data | Where-Object {$_ -like "id*"}).Split()[-1].Split('/')[0]
+            location = ($file.FullName | Select-String -Pattern 'codeql.*').Matches.Value
+        }
+        [array]$report += $dataObj
+    }
+}
+$report | Export-Csv -Path CodeQLSecuritySeverityRules.csv -Force
